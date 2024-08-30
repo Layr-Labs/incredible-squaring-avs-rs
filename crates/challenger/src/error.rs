cc5705cf9565a37c9727832a284b5d4933ead63a
supernovahs <supernovahs@proton.me>
2024-08-30 20:51:49 +0530

    clippy:challenger

--- crates/challenger/src/error.rs
+++ crates/challenger/src/error.rs
@@ -7,30 +7,39 @@ use thiserror::Error;
 /// Error returned by chainio
 #[derive(Debug, Error)]
 pub enum ChallengerError {
+    /// Chain Io crate error
     #[error("Chain Io crate error : {0}")]
     ChainIo(#[from] ChainIoError),
 
+    /// Alloy contract error
     #[error("Alloy contract error: {0}")]
     AlloyContractError(#[from] AlloyError),
 
+    /// Task Response is correct
     #[error("Task Response is not wrong")]
     TaskResponseisCorrect,
 
+    /// Task Response not found
     #[error("Task Response not found")]
     TaskResponseNotFound,
 
-    #[error("Task Response not found")]
+    /// Task not found
+    #[error("Task not found")]
     TaskNotFound,
 
+    /// Alloy sol types error
     #[error("Alloy sol types error :{0}")]
     AlloySolType(#[from] AlloySolTypeError),
 
+    /// Transaction hash not found
     #[error("Tx hash not found")]
     TransactionHashNotFound,
 
+    /// Decoded data empty
     #[error("Decoded data empty")]
     EmptyDecodedData,
 
+    /// Failed to decode event
     #[error("Failed to decode event ")]
     DecodeEvent,
 
@@ -38,6 +47,7 @@ pub enum ChallengerError {
     #[error("Failed to parse ecdsa keystore signer")]
     ECDSAKeystoreSigner,
 
+    /// Config error
     #[error("Config error {0}")]
     ConfigParseError(#[from] ConfigError),
 }
