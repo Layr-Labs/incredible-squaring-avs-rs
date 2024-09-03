77f3a03fc62e0c0ae30534c313812a7e58918d71
supernovahs <supernovahs@proton.me>
2024-09-03 12:37:38 +0530

    add test for incredible metrics

--- crates/metrics/src/lib.rs
+++ crates/metrics/src/lib.rs
@@ -22,3 +22,41 @@ pub fn increment_num_tasks_received() {
 pub fn inc_num_tasks_accepted_by_aggregator() {
     counter!("num_signed_task_responses_accepted_by_aggregator").increment(1);
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use eigen_metrics::prometheus::init_registry;
+    use std::{net::SocketAddr, time::Duration};
+
+    #[tokio::test]
+    async fn test_prometheus_server() {
+        let socket: SocketAddr = "127.0.0.1:9091".parse().unwrap();
+        init_registry(socket);
+
+        // Initialize  incredible squaring metrics
+        new();
+
+        tokio::time::sleep(Duration::from_secs(1)).await;
+
+        async fn get_metrics_body(client: &reqwest::Client, url: &str) -> String {
+            let resp = client.get(url).send().await.unwrap();
+            resp.text().await.unwrap()
+        }
+        let client = reqwest::Client::new();
+        let _ = client
+            .get("http://127.0.0.1:9091/metrics")
+            .send()
+            .await
+            .unwrap();
+
+        increment_num_tasks_received();
+        inc_num_tasks_accepted_by_aggregator();
+
+        let body = get_metrics_body(&client, "http://127.0.0.1:9091/metrics").await;
+
+        assert!(body.contains("num_tasks_received 1"));
+        assert!(body.contains("num_signed_task_responses_accepted_by_aggregator 1"));
+    }
+}
