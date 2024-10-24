6a81ac8734c18b27268267e65a3562aa50c73b73
Pablo Deymonnaz <deymonnaz@gmail.com>
2024-10-24 18:20:16 -0300

    demo for telemetry

--- bin/incredible-squaring-avs/src/main.rs
+++ bin/incredible-squaring-avs/src/main.rs
@@ -1,6 +1,9 @@
 #![allow(missing_docs)]
 
 fn main() {
+    let _ = eigen_telemetry::telemetry::Telemetry::set_config("@@@@", "@@@@");
+    eigen_telemetry::telemetry::Telemetry::capture_event("started").unwrap();
+
     use incredible_squaring_avs::cli::Cli;
 
     if let Err(err) = Cli::parse_args().start() {
@@ -8,3 +11,12 @@ fn main() {
         std::process::exit(1);
     }
 }
+
+/*
+    let client = posthog_rs::client("@@@@");
+
+    let _handler = std::thread::spawn(move || {
+        let event = posthog_rs::Event::new("app_started_manual", "@@@@");
+        client.capture(event).unwrap();
+    });
+*/
