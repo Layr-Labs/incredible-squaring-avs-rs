a8668e46edf3a6629fc0f482e16cdb48355f58e3
supernovahs <supernovahs@proton.me>
2024-11-08 20:04:07 +0530

    hit and trial

--- contracts/anvil/deploy-eigenlayer.sh
+++ contracts/anvil/deploy-eigenlayer.sh
@@ -12,4 +12,4 @@ root_dir=$(realpath "$parent_path/../..")
 # Deploy Contracts
 cd "$root_dir/contracts"
 forge create src/ContractsRegistry.sol:ContractsRegistry --rpc-url $RPC_URL --private-key $PRIVATE_KEY
-forge script script/DeployEigenlayerCore.s.sol:DeployEigenlayerCore --rpc-url $RPC_URL --broadcast --slow
+# forge script script/DeployEigenlayerCore.s.sol:DeployEigenlayerCore --rpc-url $RPC_URL --broadcast --slow
