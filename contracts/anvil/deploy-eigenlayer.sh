6cbf25d9699eb54f9e12c660aaf4ec80d32d11f2
supernovahs <supernovahs@proton.me>
2024-11-08 13:08:34 +0530

    update deploy-eigenlayer.sh

--- contracts/anvil/deploy-eigenlayer.sh
+++ contracts/anvil/deploy-eigenlayer.sh
@@ -2,22 +2,18 @@
 
 RPC_URL=http://localhost:8545
 PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
-# cd to the directory of this script so that this can be run from anywhere
-parent_path=$(
-    cd "$(dirname "${BASH_SOURCE[0]}")"
-    pwd -P
-)
-# At this point we are in tests/anvil
+
+# Navigate to the script directory
+parent_path=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
 cd "$parent_path"
 
-root_dir=$(realpath $parent_path/../..)
+root_dir=$(realpath "$parent_path/../..")
 
-# DEPLOY CONTRACT REGISTRY
-cd $root_dir/contracts
+# Ensure dependencies are installed
+cd "$root_dir/contracts"
+forge install
+forge update
+
+# Deploy Contracts
 forge create ContractsRegistry --rpc-url $RPC_URL --private-key $PRIVATE_KEY
-forge script script/DeployEigenlayerCore.s.sol --rpc-url http://localhost:8545 --broadcast --slow
-
-
-
-
-
+forge script contracts/script/DeployEigenlayerCore.s.sol:DeployEigenlayerCore --rpc-url $RPC_URL --broadcast --slow
