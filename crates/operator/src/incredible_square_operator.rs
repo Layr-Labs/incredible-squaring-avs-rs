1e0c348990a5c7fd0071e7577f5f679ea59a2f38
Pablo Deymonnaz <deymonnaz@gmail.com>
2025-01-30 19:23:20 -0300

    starting including operator using common functions from sdk

--- crates/operator/src/incredible_square_operator.rs
+++ crates/operator/src/incredible_square_operator.rs
@@ -1,57 +1,81 @@
-use std::sync::Arc;
-
-use crate::client::ClientAggregator;
-use crate::error::OperatorError;
-use alloy::{
-    primitives::{keccak256, Address},
-    providers::WsConnect,
-    rpc::types::Filter,
-    sol_types::{SolEvent, SolValue},
-};
-
-use alloy::primitives::U256;
-use alloy::providers::{Provider, ProviderBuilder};
-use eigen_client_avsregistry::reader::AvsRegistryChainReader;
+use alloy::primitives::Address;
+use eigen_aggregator::traits::TaskResponse;
 use eigen_client_eth::instrumented_client::InstrumentedClient;
 use eigen_crypto_bls::BlsKeyPair;
-use eigen_logging::get_logger;
+use eigen_operator::traits::Operator;
+use eigen_operator::{client::ClientAggregator, error::OperatorError};
+use eigen_types::avs::TaskIndex;
 use eigen_types::operator::OperatorId;
 use eyre::Result;
-use futures_util::StreamExt;
-use incredible_aggregator::SignedTaskResponse;
-use incredible_bindings::incrediblesquaringtaskmanager::IIncredibleSquaringTaskManager::TaskResponse;
-use incredible_bindings::incrediblesquaringtaskmanager::IncredibleSquaringTaskManager::{
-    self, NewTaskCreated,
-};
+use incredible_bindings::incrediblesquaringtaskmanager::IIncredibleSquaringTaskManager::TaskResponse as SolTaskResponse;
+use incredible_bindings::incrediblesquaringtaskmanager::IncredibleSquaringTaskManager::NewTaskCreated;
 use incredible_config::IncredibleConfig;
 use rust_bls_bn254::keystores::base_keystore::Keystore;
-use tracing::info;
+use serde::{Deserialize, Serialize};
 
 /// Main Operator
 #[derive(Debug)]
-pub struct OperatorBuilder {
-    http_rpc_url: String,
-
-    ws_rpc_url: String,
-
-    operator_addr: Address,
-
-    key_pair: BlsKeyPair,
-
-    operator_id: OperatorId,
+pub struct IncredibleSquareOperator {
+    /// HTTP RPC URL
+    pub http_rpc_url: String,
+    /// WS RPC URL
+    pub ws_rpc_url: String,
+    /// Operator Address
+    pub operator_addr: Address,
+    /// [`BlsKeyPair`]
+    pub key_pair: BlsKeyPair,
+    /// Operator ID
+    pub operator_id: OperatorId,
     /// [`ClientAggregator`]
     pub client: ClientAggregator,
-
-    registry_coordinator: Address,
-
-    operator_state_retriever: Address,
-
-    slash_simulate: bool,
+    /// Registry Coordinator Address
+    pub registry_coordinator: Address,
+    /// Operator State Retriever Address
+    pub operator_state_retriever: Address,
 }
 
-impl OperatorBuilder {
+#[derive(Debug, Serialize, Deserialize)]
+/// Task Response for Incredible Square Operator
+pub struct IncredibleTaskResponse(SolTaskResponse);
+
+impl TaskResponse for IncredibleTaskResponse {
+    fn task_index(&self) -> TaskIndex {
+        self.0.referenceTaskIndex
+    }
+    fn digest(&self) -> alloy::primitives::B256 {
+        todo!()
+    }
+}
+
+impl Operator for IncredibleSquareOperator {
+    type TaskResponse = IncredibleTaskResponse;
+    type NewTaskEvent = NewTaskCreated;
+
+    fn process_new_task(new_task_created: NewTaskCreated) -> Self::TaskResponse {
+        #[allow(unused_mut)]
+        #[allow(unused_assignments)]
+        let mut number_to_be_squared = new_task_created.task.numberToBeSquared;
+
+        #[cfg(feature = "integration_tests")]
+        {
+            use tracing::info;
+
+            number_to_be_squared = alloy::primitives::U256::from(9);
+            info!("Challenger test: setting number to be squared to 9");
+        }
+
+        let num_squared = number_to_be_squared * number_to_be_squared;
+
+        IncredibleTaskResponse(SolTaskResponse {
+            referenceTaskIndex: new_task_created.taskIndex,
+            numberSquared: num_squared,
+        })
+    }
+}
+
+impl IncredibleSquareOperator {
     /// Build the Operator Builder
-    pub async fn build(config: IncredibleConfig) -> Result<Self, OperatorError> {
+    pub async fn new(config: IncredibleConfig) -> Result<Self, OperatorError> {
         let _instrumented_client = InstrumentedClient::new(&config.http_rpc_url()).await;
         // Read BlsKey from path
         let keystore = Keystore::from_file(&config.bls_keystore_path())?
@@ -66,8 +90,6 @@ impl OperatorBuilder {
         let operator_address = config.operator_address()?;
         let mut client = ClientAggregator::new(config.aggregator_ip_addr());
         let _ = client.dial_aggregator_rpc_client();
-        let slash = config.slash_simulate();
-
         Ok(Self {
             http_rpc_url: config.http_rpc_url(),
             ws_rpc_url: config.ws_rpc_url(),
@@ -77,115 +99,24 @@ impl OperatorBuilder {
             client,
             registry_coordinator: registry_coordinator_addr,
             operator_state_retriever: operator_statr_retriever_addr,
-            slash_simulate: slash,
         })
-    }
-
-    /// Get the [`BlsKeyPair`]
-    pub fn bls_key_pair(&self) -> BlsKeyPair {
-        self.key_pair.clone()
-    }
-
-    /// Processes new task
-    pub fn process_new_task(&self, new_task_created: NewTaskCreated) -> TaskResponse {
-        #[allow(unused_mut)]
-        #[allow(unused_assignments)]
-        let mut number_to_be_squared = new_task_created.task.numberToBeSquared;
-
-        #[cfg(feature = "integration_tests")]
-        {
-            number_to_be_squared = U256::from(9);
-            info!("Challenger test: setting number to be squared to 9");
-        }
-
-        let num_squared = if self.slash_simulate {
-            U256::from(28) // not a perfect square, so it can't be correct in any input
-        } else {
-            number_to_be_squared * number_to_be_squared
-        };
-
-        TaskResponse {
-            referenceTaskIndex: new_task_created.taskIndex,
-            numberSquared: num_squared,
-        }
-    }
-
-    /// Start the operator
-    pub async fn start_operator(&mut self) -> Result<()> {
-        let avs_registry_reader = AvsRegistryChainReader::new(
-            get_logger(),
-            self.registry_coordinator,
-            self.operator_state_retriever,
-            self.http_rpc_url.clone(),
-        )
-        .await
-        .unwrap();
-        let is_registered = avs_registry_reader
-            .is_operator_registered(self.operator_addr)
-            .await?;
-        info!("is_operator1_registered {}", is_registered);
-        let arc_client = Arc::new(self.client.clone());
-        if is_registered {
-            info!("Starting operator");
-
-            let ws = WsConnect::new(self.ws_rpc_url.clone());
-            let provider = ProviderBuilder::new().on_ws(ws).await?;
-
-            let filter = Filter::new().event_signature(NewTaskCreated::SIGNATURE_HASH);
-            let sub = provider.subscribe_logs(&filter).await?;
-            let mut stream = sub.into_stream();
-
-            while let Some(log) = stream.next().await {
-                let task_option = log
-                    .log_decode::<IncredibleSquaringTaskManager::NewTaskCreated>()
-                    .ok();
-                if let Some(task) = task_option {
-                    let data = task.data();
-                    let new_task_created = NewTaskCreated {
-                        task: data.task.clone(),
-                        taskIndex: data.taskIndex,
-                    };
-                    info!(
-                        "operator1 picked up a new task , index: {} ",
-                        data.taskIndex
-                    );
-                    incredible_metrics::increment_num_tasks_received();
-                    let task_response = self.process_new_task(new_task_created);
-                    let signed_task_response = self.sign_task_response(task_response)?;
-                    let _ = arc_client
-                        .send_signed_task_response(signed_task_response)
-                        .await;
-                }
-            }
-        }
-        Ok(())
-    }
-
-    /// Sign the task response
-    pub fn sign_task_response(
-        &self,
-        task_response: TaskResponse,
-    ) -> Result<SignedTaskResponse, OperatorError> {
-        let encoded_response = TaskResponse::abi_encode(&task_response);
-        let hash_msg = keccak256(encoded_response);
-
-        let signed_msg = self.key_pair.sign_message(&hash_msg);
-        let signed_task_response =
-            SignedTaskResponse::new(task_response, signed_msg, self.operator_id);
-        Ok(signed_task_response)
     }
 }
 
 #[cfg(test)]
 mod tests {
-
     use super::*;
-    use alloy::primitives::Bytes;
-    use alloy::primitives::U256;
+    use alloy::{
+        primitives::{keccak256, Bytes, U256},
+        sol_types::SolValue,
+    };
     use ark_ec::AffineRepr;
     use ark_ff::fields::PrimeField;
     use eigen_crypto_bn254::utils::verify_message;
+    use eigen_operator::traits::Operator;
     use incredible_bindings::incrediblesquaringtaskmanager::IIncredibleSquaringTaskManager::Task;
+    use incredible_bindings::incrediblesquaringtaskmanager::IIncredibleSquaringTaskManager::TaskResponse;
+    use incredible_bindings::incrediblesquaringtaskmanager::IncredibleSquaringTaskManager::NewTaskCreated;
     use incredible_testing_utils::{
         get_incredible_squaring_operator_state_retriever,
         get_incredible_squaring_registry_coordinator,
@@ -215,7 +146,9 @@ mod tests {
                 .await
                 .to_string(),
         );
-        let operator_builder = OperatorBuilder::build(incredible_config).await.unwrap();
+        let operator_builder = IncredibleSquareOperator::new(incredible_config)
+            .await
+            .unwrap();
 
         assert_eq!(
             U256::from_limbs(
@@ -258,12 +191,11 @@ mod tests {
                 .await
                 .to_string(),
         );
-        let operator_builder = OperatorBuilder::build(incredible_config).await.unwrap();
 
-        let task_response = operator_builder.process_new_task(new_task_created);
+        let task_response = IncredibleSquareOperator::process_new_task(new_task_created);
 
-        assert_eq!(task_response.numberSquared, U256::from(16));
-        assert_eq!(task_response.referenceTaskIndex, 1u32);
+        assert_eq!(task_response.0.numberSquared, U256::from(16));
+        assert_eq!(task_response.0.referenceTaskIndex, 1u32);
     }
 
     #[tokio::test]
@@ -279,7 +211,9 @@ mod tests {
                 .await
                 .to_string(),
         );
-        let _ = OperatorBuilder::build(incredible_config).await.unwrap();
+        let _ = IncredibleSquareOperator::new(incredible_config)
+            .await
+            .unwrap();
     }
 
     #[tokio::test]
@@ -300,18 +234,23 @@ mod tests {
                 .await
                 .to_string(),
         );
-        let operator_builder = OperatorBuilder::build(incredible_config).await.unwrap();
-        let signed_task_response = operator_builder
-            .sign_task_response(task_response.clone())
+        let operator_builder = IncredibleSquareOperator::new(incredible_config)
+            .await
             .unwrap();
+        let signed_task_response = IncredibleSquareOperator::sign_task_response(
+            &operator_builder.key_pair,
+            &operator_builder.operator_id,
+            super::IncredibleTaskResponse(task_response.clone()),
+        )
+        .unwrap();
 
-        let bls_key_pair = operator_builder.bls_key_pair();
+        let bls_key_pair = &operator_builder.key_pair;
         let encoded_response = TaskResponse::abi_encode(&task_response);
         let hash_msg = keccak256(encoded_response);
         assert!(verify_message(
             bls_key_pair.public_key_g2().g2(),
             &hash_msg,
-            signed_task_response.signature.g1_point().g1()
+            signed_task_response.signature().g1_point().g1()
         ));
     }
 }
