1cdb49361227dded95c394a674919e48371af4f2
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-03-25 11:51:08 -0300

    refactor: remove unused fields in RPCRquest

--- crates/aggregator/src/rpc_server.rs
+++ crates/aggregator/src/rpc_server.rs
@@ -15,8 +15,6 @@ pub struct SignedTaskResponse {
 /// RPC Request is used to parse the request from the RPC server
 #[derive(Debug, Serialize, Deserialize)]
 pub struct RpcRequest {
-    id: u64,
-    jsonrpc: String,
     /// Params of the request
     pub params: SignedTaskResponse,
 }
