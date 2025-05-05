d76b7d54b5632f16e8be7324ab3f51857650d02e
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-05-05 17:03:07 -0300

    feat: implement task manager trait to aggregator

--- crates/avs/src/builder/mod.rs
+++ crates/avs/src/builder/mod.rs
@@ -1,14 +1,16 @@
 //! Builder module for the AVS. Starts all the services for the AVS using futures simulatenously.
-use eigensdk::nodeapi::{NodeApi, NodeInfo};
+use alloy::{
+    network::EthereumWallet, providers::ProviderBuilder, signers::local::PrivateKeySigner,
+    transports::http::reqwest::Url,
+};
+use eigensdk::{
+    aggregator::{Aggregator, AggregatorConfig},
+    nodeapi::{NodeApi, NodeInfo},
+};
 use futures::TryFutureExt;
-use incredible_aggregator::Aggregator;
-use incredible_challenger::Challenger;
+use incredible_bindings::incrediblesquaringtaskmanager::IncredibleSquaringTaskManager;
 use incredible_config::IncredibleConfig;
-use incredible_operator::builder::OperatorBuilder;
-use incredible_operator_2::builder::OperatorBuilder as OperatorBuilder2;
-use incredible_task_generator::TaskManager;
-use ntex::rt::System;
-use std::{future::Future, sync::Arc};
+use std::{future::Future, str::FromStr, sync::Arc};
 use tracing::info;
 /// Launch Avs trait
 pub trait LaunchAvs<T: Send + 'static> {
@@ -48,62 +50,84 @@ impl Default for DefaultAvsLauncher {
 
 impl LaunchAvs<AvsBuilder> for DefaultAvsLauncher {
     async fn launch_avs(self, avs: AvsBuilder) -> eyre::Result<()> {
-        info!("launching crates: incredible-squaring-avs-rs");
-        incredible_metrics::new();
-        // start operator
-        let mut operator_builder = OperatorBuilder::build(avs.config.clone()).await?;
-        let mut operator_builder2 = OperatorBuilder2::build(
-            avs.config.clone(),
-            Some(Arc::new(operator_builder.client.clone())),
-        )
-        .await?;
+        // info!("launching crates: incredible-squaring-avs-rs");
+        // incredible_metrics::new();
+        // // start operator
+        // let mut operator_builder = OperatorBuilder::build(avs.config.clone()).await?;
+        // let mut operator_builder2 = OperatorBuilder2::build(
+        //     avs.config.clone(),
+        //     Some(Arc::new(operator_builder.client.clone())),
+        // )
+        // .await?;
 
-        let mut challenge = Challenger::build(avs.config.clone()).await?;
-        let operator_service = operator_builder
-            .start_operator()
-            .map_err(|e| eyre::eyre!("Operator error: {:?}", e));
+        // let mut challenge = Challenger::build(avs.config.clone()).await?;
+        // let operator_service = operator_builder
+        //     .start_operator()
+        //     .map_err(|e| eyre::eyre!("Operator error: {:?}", e));
 
-        let operator2_service = operator_builder2
-            .start_operator()
-            .map_err(|e| eyre::eyre!("Operator error: {:?}", e));
+        // let operator2_service = operator_builder2
+        //     .start_operator()
+        //     .map_err(|e| eyre::eyre!("Operator error: {:?}", e));
 
-        let challenger_service = challenge
-            .start_challenger()
-            .map_err(|e| eyre::eyre!("Challenger error: {:?}", e));
-        let aggregator = Aggregator::new(avs.config.clone()).await?;
+        // let challenger_service = challenge
+        //     .start_challenger()
+        //     .map_err(|e| eyre::eyre!("Challenger error: {:?}", e));
+        // let aggregator = Aggregator::new(avs.config.clone()).await?;
+        // let aggregator_service_with_rpc_client = aggregator
+        //     .start(avs.config.ws_rpc_url().clone())
+        //     .map_err(|e| eyre::eyre!("Aggregator error {e:?}"));
+
+        // let task_manager = TaskManager::new(
+        //     avs.config.task_manager_addr()?,
+        //     avs.config.http_rpc_url(),
+        //     avs.config.task_manager_signer(),
+        //     avs.config.quorum_number()?.to_string(),
+        // );
+        // let task_spam_service = task_manager
+        //     .start()
+        //     .map_err(|e| eyre::eyre!("Task manager error {e:?}"));
+        // let node_info = NodeInfo::new("incredible-squaring", "v0.0.1");
+        // let node_api = NodeApi::new(node_info);
+        // let node_api_address = avs.config.node_api_port_address();
+        // info!("node_api_address{:?}", node_api_address);
+
+        // std::thread::spawn(move || {
+        //     let _ = System::new("node_api_system").block_on(async move {
+        //         let node_api_server = node_api.start_server(node_api_address.as_str()).unwrap();
+        //         node_api_server.await
+        //     });
+        // });
+
+        // let _ = futures::future::try_join5(
+        //     operator_service,
+        //     operator2_service,
+        //     challenger_service,
+        //     aggregator_service_with_rpc_client,
+        //     task_spam_service,
+        // )
+        // .await?;
+
+        // Create task manager contract instance
+        let url = Url::parse(&avs.config.http_rpc_url())?;
+        let signer = PrivateKeySigner::from_str(&avs.config.get_signer())?;
+        let wallet = EthereumWallet::new(signer);
+        let pr = ProviderBuilder::new().wallet(wallet).on_http(url);
+        let task_manager_contract =
+            IncredibleSquaringTaskManager::new(avs.config.task_manager_addr()?, pr);
+
+        // Launch aggregator
+        let config_aggregator = AggregatorConfig {
+            http_rpc_url: avs.config.http_rpc_url().to_string(),
+            ws_rpc_url: avs.config.ws_rpc_url().to_string(),
+            server_address: avs.config.aggregator_ip_addr().to_string(),
+            registry_coordinator: avs.config.registry_coordinator_addr()?,
+            operator_state_retriever: avs.config.operator_state_retriever_addr()?,
+        };
+
+        let aggregator = Aggregator::new(config_aggregator, task_manager_contract).await?;
         let aggregator_service_with_rpc_client = aggregator
-            .start(avs.config.ws_rpc_url().clone())
-            .map_err(|e| eyre::eyre!("Aggregator error {e:?}"));
-
-        let task_manager = TaskManager::new(
-            avs.config.task_manager_addr()?,
-            avs.config.http_rpc_url(),
-            avs.config.task_manager_signer(),
-            avs.config.quorum_number()?.to_string(),
-        );
-        let task_spam_service = task_manager
             .start()
-            .map_err(|e| eyre::eyre!("Task manager error {e:?}"));
-        let node_info = NodeInfo::new("incredible-squaring", "v0.0.1");
-        let node_api = NodeApi::new(node_info);
-        let node_api_address = avs.config.node_api_port_address();
-        info!("node_api_address{:?}", node_api_address);
-
-        std::thread::spawn(move || {
-            let _ = System::new("node_api_system").block_on(async move {
-                let node_api_server = node_api.start_server(node_api_address.as_str()).unwrap();
-                node_api_server.await
-            });
-        });
-
-        let _ = futures::future::try_join5(
-            operator_service,
-            operator2_service,
-            challenger_service,
-            aggregator_service_with_rpc_client,
-            task_spam_service,
-        )
-        .await?;
+            .map_err(|e| eyre::eyre!("Aggregator error {e:?}"));
 
         Ok(())
     }
--- crates/avs/src/lib.rs
+++ crates/avs/src/lib.rs
@@ -1,2 +1,196 @@
 //! Starts all the services for the AVS using futures
 pub mod builder;
+
+use alloy::{
+    contract::private::{Provider, Transport},
+    network::Network,
+    primitives::U256,
+};
+use eigensdk::{
+    challenger::task_manager::TaskManagerError,
+    task_processor::{task::Task, task_response::TaskResponse},
+    utils::slashing::middleware::iblssignaturechecker::IBLSSignatureCheckerTypes::NonSignerStakesAndSignature,
+};
+use incredible_bindings::incrediblesquaringtaskmanager::{
+    IBLSSignatureCheckerTypes::NonSignerStakesAndSignature as ContractNonSignerStakesAndSignature,
+    IIncredibleSquaringTaskManager::{Task as ContractTask, TaskResponse as ContractTaskResponse},
+    IncredibleSquaringTaskManager::{IncredibleSquaringTaskManagerInstance, NewTaskCreated},
+    BN254::{G1Point, G2Point},
+};
+
+pub struct TaskManagerWrapper<T, P, N>(pub IncredibleSquaringTaskManagerInstance<T, P, N>);
+
+impl<T, P, N> eigensdk::task_processor::task_manager::TaskManagerContract<T, P, N>
+    for TaskManagerWrapper<T, P, N>
+where
+    T: Transport + Clone + Send + Sync,
+    P: Provider<T, N>,
+    N: Network,
+{
+    type Input = U256;
+    type Output = U256;
+    type NewTaskEvent = NewTaskCreated;
+
+    async fn respond_to_task(
+        &self,
+        task: Task<Self::Input>,
+        response: TaskResponse<Self::Output>,
+        non_signer_stakes_and_signature: NonSignerStakesAndSignature,
+    ) -> Result<(), TaskManagerError> {
+        let contract_task = ContractTask {
+            numberToBeSquared: task.input,
+            taskCreatedBlock: task.task_created_block,
+            quorumNumbers: task.quorum_numbers,
+            quorumThresholdPercentage: task.quorum_threshold_percentage,
+        };
+
+        let contract_response = ContractTaskResponse {
+            numberSquared: response.response,
+            referenceTaskIndex: response.task_index,
+        };
+
+        let apk_g2 = G2Point {
+            X: non_signer_stakes_and_signature.apkG2.X,
+            Y: non_signer_stakes_and_signature.apkG2.Y,
+        };
+
+        let sigma = G1Point {
+            X: non_signer_stakes_and_signature.sigma.X,
+            Y: non_signer_stakes_and_signature.sigma.Y,
+        };
+
+        let quorum_apks = non_signer_stakes_and_signature
+            .quorumApks
+            .iter()
+            .map(|apk| G1Point { X: apk.X, Y: apk.Y })
+            .collect();
+
+        let non_signer_pubkeys = non_signer_stakes_and_signature
+            .nonSignerPubkeys
+            .iter()
+            .map(|pubkey| G1Point {
+                X: pubkey.X,
+                Y: pubkey.Y,
+            })
+            .collect();
+
+        let non_signer_stakes_and_signature = ContractNonSignerStakesAndSignature {
+            nonSignerStakeIndices: non_signer_stakes_and_signature.nonSignerStakeIndices,
+            nonSignerQuorumBitmapIndices: non_signer_stakes_and_signature
+                .nonSignerQuorumBitmapIndices,
+            quorumApkIndices: non_signer_stakes_and_signature.quorumApkIndices,
+            totalStakeIndices: non_signer_stakes_and_signature.totalStakeIndices,
+            apkG2: apk_g2,
+            quorumApks: quorum_apks,
+            nonSignerPubkeys: non_signer_pubkeys,
+            sigma,
+        };
+
+        self.0
+            .respondToTask(
+                contract_task,
+                contract_response,
+                non_signer_stakes_and_signature,
+            )
+            .send()
+            .await
+            .unwrap()
+            .get_receipt()
+            .await
+            .unwrap();
+
+        Ok(())
+    }
+}
+
+// // Implement the TaskManagerContract trait for the task manager contract.
+// // You need to specify the input type of the task. In this case, U256.
+// // You also need to specify the call type of the task manager contract. `createNewTask` uses `createNewTaskCall`.
+// // You also need to specify the provider and network types.
+// //
+// // NOTE: When you are implementing this, you will have an exteranl trait `TaskManagerContract` and and external struct
+// // `CONTRACT_NAME_INSTANCE`, so it will throw an error. You can wrap the external struct in a newtype to avoid this.
+// // Example:
+// // struct TaskManagerWrapper<T, P, N>(IncredibleSquaringTaskManagerInstance<T, P, N>);
+// //
+// // impl<T, P, N> TaskManagerContract<U256, T, P, N> for TaskManagerWrapper<T, P, N> { ... }
+// impl<T, P, N> eigen_task_spammer::task_manager::TaskManagerContract<U256, T, P, N>
+//     for IncredibleSquaringTaskManagerInstance<T, P, N>
+// where
+//     T: Transport + Clone + Send + Sync,
+//     P: Provider<T, N>,
+//     N: Network,
+// {
+//     async fn create_new_task(
+//         &self,
+//         input: U256,
+//         quorum_threshold: QuorumThresholdPercentage,
+//         quorums: Vec<QuorumNum>,
+//     ) -> Result<N::ReceiptResponse, TaskSpammerError> {
+//         Ok(self
+//             .createNewTask(input, quorum_threshold.into(), quorums.into())
+//             .send()
+//             .await?
+//             .get_receipt()
+//             .await?)
+//     }
+// }
+
+// // Implement the Challenger TaskManagerContract trait for the task manager contract.
+// impl<T, P, N> eigen_challenger::task_manager::TaskManagerContract<T, P, N>
+//     for IncredibleSquaringTaskManagerInstance<T, P, N>
+// where
+//     T: Transport + Clone + Send + Sync,
+//     P: Provider<T, N>,
+//     N: Network,
+// {
+//     type Input = U256;
+//     type Output = U256;
+//     type NewTaskEvent = NewTaskCreated;
+//     type TaskRespondedEvent = TaskResponded;
+
+//     async fn raise_challenge(
+//         &self,
+//         task: Task<Self::Input>,
+//         task_response: TaskResponse<Self::Output>,
+//         task_response_metadata: TaskResponseMetadataSol,
+//         pubkeys_of_non_signing_operators: Vec<G1Point>,
+//     ) -> Result<(), TaskManagerError> {
+//         let contract_task = ContractTask {
+//             numberToBeSquared: task.input,
+//             taskCreatedBlock: task.task_created_block,
+//             quorumNumbers: task.quorum_numbers,
+//             quorumThresholdPercentage: task.quorum_threshold_percentage,
+//         };
+
+//         let contract_response = ContractTaskResponse {
+//             numberSquared: task_response.response,
+//             referenceTaskIndex: task_response.task_index,
+//         };
+
+//         let task_response_metadata = TaskResponseMetadata {
+//             taskResponsedBlock: task_response_metadata.taskResponsedBlock,
+//             hashOfNonSigners: task_response_metadata.hashOfNonSigners,
+//         };
+
+//         let pubkey_non_signer = pubkeys_of_non_signing_operators
+//             .iter()
+//             .map(|p| G1Binding { X: p.X, Y: p.Y })
+//             .collect();
+
+//         self.raiseAndResolveChallenge(
+//             contract_task,
+//             contract_response,
+//             task_response_metadata,
+//             pubkey_non_signer,
+//         )
+//         .send()
+//         .await
+//         .unwrap()
+//         .get_receipt()
+//         .await
+//         .unwrap();
+
+//         Ok(())
+//     }
+// }
