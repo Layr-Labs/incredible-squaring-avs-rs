f4b38a507ca5431b5703a90fc711e1bcb75087bc
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-05-26 15:40:02 -0300

    test: add parameters to bins

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,16 +1,56 @@
 use std::process::Stdio;
 use tokio::process::Command as TokioCommand;
 
-async fn spawn_bin(name: &str) -> tokio::process::Child {
-    tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+async fn spawn_aggregator() -> tokio::process::Child {
     TokioCommand::new("cargo")
-        .args(["run", "--bin", name])
+        .args(["run", "--bin", "incredible-squaring-aggregator"])
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
         .spawn()
         .expect("Could not spawn binary")
 }
 
+async fn spawn_challenger() -> tokio::process::Child {
+    TokioCommand::new("cargo")
+        .args(["run", "--bin", "incredible-squaring-challenger"])
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .expect("Could not spawn binary")
+}
+
+async fn spawn_operator(config_path: &str, failure_rate: u32) -> tokio::process::Child {
+    // Wait 5 seconds to let the aggregator start
+    tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+
+    TokioCommand::new("cargo")
+        .args([
+            "run",
+            "--bin",
+            "incredible-squaring-operator",
+            "--",
+            "-c",
+            config_path,
+            "-f",
+            failure_rate.to_string().as_str(),
+        ])
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .expect("Could not spawn binary")
+}
+
+async fn spawn_task_spammer() -> tokio::process::Child {
+    // Wait until operators are spawned
+    tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+
+    TokioCommand::new("cargo")
+        .args(["run", "--bin", "incredible-squaring-task-spammer"])
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .expect("Could not spawn binary")
+}
 pub mod integration_test {
     use std::str::FromStr;
 
@@ -22,14 +62,17 @@ pub mod integration_test {
 
     use super::*;
 
+    // This test spawns the aggregator, challenger, two operators, and a task spammer.
+    // The operators are configured to fail 30% and 99% of the time, respectively.
+    // So when checking `taskSuccesfullyChallenged`, we expect it to be true.
     #[tokio::test]
     async fn test_integration() {
         let mut children = vec![
-            spawn_bin("incredible-squaring-aggregator").await,
-            spawn_bin("incredible-squaring-challenger").await,
-            spawn_bin("incredible-squaring-operator").await,
-            spawn_bin("incredible-squaring-operator-2").await,
-            spawn_bin("incredible-squaring-task-spammer").await,
+            spawn_aggregator().await,
+            spawn_challenger().await,
+            spawn_operator("src/config/squaring-operator.toml", 30).await,
+            spawn_operator("src/config/squaring-operator-2.toml", 99).await,
+            spawn_task_spammer().await,
         ];
 
         // Wait 15 seconds to let operators respond to tasks
@@ -56,16 +99,12 @@ pub mod integration_test {
             .unwrap()
             ._0;
 
-        dbg!("Latest task num: {}", latest_task_num);
-
         let task_hash = task_manager_contract
             .allTaskHashes(latest_task_num - 1)
             .call()
             .await
             .unwrap()
             ._0;
-
-        dbg!("Task hash: {:?}", task_hash);
 
         assert_ne!(FixedBytes::<32>::default(), task_hash);
 
